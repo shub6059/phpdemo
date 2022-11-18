efc900fd1f9e70f56ef5bee8996445dea6466c0b
Shubham Patil <68894316+shub6059@users.noreply.github.com>
2022-11-18 15:56:28 +0530

    update

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,5 +1,5 @@
 <?php
-    $con=mysqli_connect("localhost", "root", "", "vehicle");
+    $con=mysqli_connect("ls-ec9f409eca9c895cc24edbf4e66ff14fc2b05495.ca0vjms4jkx7.ap-south-1.rds.amazonaws.com:3306", "dbmasteruser", "IGS(OvWVsa}>--f`L2(ZD(eTG1v;1B1Y", "vps");
     if(mysqli_connect_errno()){
     echo "Connection Fail".mysqli_connect_error();
     }
